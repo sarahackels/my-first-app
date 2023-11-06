6d3403cf5bafed577789ac0164ba1fd643ddb38a
sarahackels <107145319+sarahackels@users.noreply.github.com>
2023-11-06 17:38:43 -0500

    Refactor latest date and rate

--- app/unemployment.py
+++ app/unemployment.py
@@ -41,7 +41,13 @@ data = parsed_response["data"]
 print("-------------------------")
 print("LATEST UNEMPLOYMENT RATE:")
 #print(data[0])
-print(f"{data[0]['value']}%", "as of", data[0]["date"])
+
+
+latest_rate = data[0]['value']
+latest_date = data[0]["date"]
+
+
+print(f"{latest_rate}%", "as of", latest_date)
 
 
 # Challenge B
@@ -73,12 +79,12 @@ fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", 
 fig.show()
 
 
+## email sending
 
 user_address = input("Enter your email address: ")
 
 
-latest_rate = data[0]['value']
-latest_date = data[0]["date"]
+
 
 content = f"""
 <h1> Unemployment Report Email </h1>
